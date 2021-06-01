f0b340cbe091953c7ae781a8404368e71b10a6b9
liudeng129 <83198851+liudeng129@users.noreply.github.com>
2021-06-02 00:22:42 +0800

    Site updated: 2021-06-02 00:22:39

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://cdn.statically.io/gh/liudeng129/album/main/min_photos/' + data.link[i];
-          var src = 'https://cdn.statically.io/gh/liudeng129/album/main/photos/' + data.link[i];
+          var minSrc = 'https://cdn.jsdelivr.net/gh/liudeng129/album@main/min_photos/' + data.link[i];
+          var src = 'https://cdn.jsdelivr.net/gh/liudeng129/album@main/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
